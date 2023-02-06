40710a3c072382dd3387756f44f6b3097b85336a
单浩辉 <1203794209@qq.com>
2023-02-06 11:00:10 +0800

    useState

---
+++ src/test.js
@@ -0,0 +1,82 @@
+let isMount = true
+let workInProgressHook = null
+
+const fiber = {
+  stateNode: App,
+  memoizedState: null
+}
+
+function useState(initialState) {
+  let hook;
+  if (isMount) {
+    hook = {
+      memoizedState: initialState,
+      next: null,
+      queue: {
+        pending: null
+      }
+    }
+    if(!fiber.memoizedState) {
+      fiber.memoizedState = hook
+    } else {
+      workInProgressHook.next = hook
+    }
+    workInProgressHook = hook
+  } else {
+    hook = workInProgressHook
+    workInProgressHook = workInProgressHook.next
+  }
+  let baseState = hook.memoizedState
+  if (hook.queue.pending) {
+    let firstUpdate = hook.queue.pending.next
+    do {
+      const action = firstUpdate.action
+      baseState = action(baseState)
+      firstUpdate = firstUpdate.next
+    } while (firstUpdate !== hook.queue.pending.next)
+    hook.queue.pending = null
+  }
+  hook.memoizedState = baseState
+  return [baseState, dispatchAction.bind(null, hook.queue)]
+}
+function dispatchAction(queue,action) {
+  const update = {
+    action,
+    next: null
+  }
+  if (queue.pending === null) {
+    // u0 => u0 => u0
+    update.next = update
+  } else {
+    // u1 => u0 => u1
+    update.next = queue.pending.next
+    queue.pending.next = update
+  }
+  queue.pending = update
+  schedule()
+} 
+
+function schedule() {
+  workInProgressHook = fiber.memoizedState
+  const app = fiber.stateNode()
+  isMount = false
+  return app
+}
+
+function App() {
+  const [num, updateNum] = useState(0)
+  const [num2, updateNum2] = useState(0)
+  console.log(num, 'num')
+  console.log(num2, 'num2')
+  return {
+    onClick() {
+      updateNum(num => num + 1)
+    },
+    onClick2() {
+      updateNum2(num => num + 1)
+    }
+  }
+}
+
+window.app = schedule()
+
